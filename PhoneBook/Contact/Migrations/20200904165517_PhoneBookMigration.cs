4aedbb59b823c07fff3ce214f1a6a4daa044d54c
Dmax007 <maxwelldexter31@yahoo.com>
2020-09-04 17:01:00 +0000

    Created the migration

---
+++ PhoneBook/Contact/Migrations/20200904165517_PhoneBookMigration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Contact.Migrations
+{
+    public partial class PhoneBookMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Phones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(maxLength: 50, nullable: false),
+                    LonestarNumber = table.Column<string>(maxLength: 20, nullable: false),
+                    OrangeNumber = table.Column<string>(maxLength: 20, nullable: false),
+                    Email = table.Column<string>(maxLength: 150, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Phones", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Phones");
+        }
+    }
+}
