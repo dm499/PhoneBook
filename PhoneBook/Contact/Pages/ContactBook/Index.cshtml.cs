a7c7fa7879e49c4a8e1a6c8d3d8ea6e588909376
Dmax007 <maxwelldexter31@yahoo.com>
2020-09-04 17:25:49 +0000

    Added the index page

---
+++ PhoneBook/Contact/Pages/ContactBook/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Contact.Data;
+using Contact.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contact.Pages.ContactBook
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Contact.Data.ApplicationDbContext _db;
+
+        public IndexModel(Contact.Data.ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<PhoneContact> Phones { get; set; }
+
+        public async Task OnGet()
+        {
+            Phones = await _db.Phones.ToListAsync();
+        }
+    }
+}
