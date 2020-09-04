0cdcfe717590eda14648c535f1a07a2bcfa661e8
Dmax007 <maxwelldexter31@yahoo.com>
2020-09-04 16:11:39 +0000

    Adding the initial built project

---
+++ PhoneBook/Contact/Data/ApplicaionDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Contact.Data
+{
+    public class ApplicaionDbContext : DbContext
+    {
+        public ApplicaionDbContext(DbContextOptions<ApplicaionDbContext> options)
+            : base (options)
+        {
+        }
+    }
+}
